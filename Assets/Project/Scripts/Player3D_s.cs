b3d72feb3b8efeadadee62068d9ed08f0d95fc9b
calvemoe <thephonep@gmail.com>
2018-11-26 13:56:26 +0200

    Test commit after .gitignore was added

---
+++ Assets/Project/Scripts/Player3D_s.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3D_s : MonoBehaviour {
+
+    //player parameters
+    public float speed;
+    public float invincibilityDuration;
+    public float speedUpDuration;
+
+    //powerups settings
+    bool usedInvincibility;
+    bool usedSpeedUp;
+    float invincibilityTimer;
+    float speedUpTimer;
+
+	// Use this for initialization
+	void Start () {
+ 
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        float speedMultiplier = speedUpTimer > 0 ? 2f : 1f;
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 relativePosition = new Vector2(Input.mousePosition.x / Screen.width, Input.mousePosition.y / Screen.height);
+
+            if (relativePosition.x < 0.5f)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(-speed * speedMultiplier, 0, 0);
+            }
+            else if (relativePosition.x > 0.5f)
+            {
+                GetComponent<Rigidbody>().velocity = new Vector3(speed * speedMultiplier, 0, 0);
+            }
+        }
+        else
+        {
+            GetComponent<Rigidbody>().velocity = Vector3.zero;
+        }
+
+        //keep player within bounce
+        float zDistance = Camera.main.transform.position.z - transform.position.z;
+
+        float leftLimit = Camera.main.ScreenToWorldPoint(new Vector3(
+            0, 
+            0, 
+            -zDistance / (Mathf.Cos(Camera.main.transform.localEulerAngles.x * Mathf.Deg2Rad))
+        )).x;
+
+        float rightLimit = Camera.main.ScreenToWorldPoint(new Vector3(
+            Screen.width,
+            0,
+            -zDistance / (Mathf.Cos(Camera.main.transform.localEulerAngles.x * Mathf.Deg2Rad))
+        )).x;
+
+        if (transform.position.x < leftLimit)
+        {
+            transform.position = new Vector3(leftLimit, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > rightLimit)
+        {
+            transform.position = new Vector3(rightLimit, transform.position.y, transform.position.z);
+        }
+
+        //using powerups
+        if (invincibilityTimer > 0f)
+        {
+            invincibilityTimer -= Time.deltaTime;
+            transform.localScale = new Vector3(1.2f, 1.2f, 1.2f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        if (speedUpTimer > 0f)
+        {
+            speedUpTimer -= Time.deltaTime;
+        }
+    }
+
+    public void ActivateInvincibility()
+    {
+        if (usedInvincibility)
+            return;
+
+        usedInvincibility = true;
+
+        invincibilityTimer = invincibilityDuration;
+    }
+
+    public void ActivateSpeedUp()
+    {
+        if (usedSpeedUp)
+            return;
+
+        usedSpeedUp = true;
+
+        speedUpTimer = speedUpDuration;
+    }
+}
---
+++ Assets/Project/Scripts/SceneManager3DSpace1.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SceneManager3DSpace1 : MonoBehaviour {
+
+    public Text infoText;
+
+    public Player3D_s player;
+
+	// Use this for initialization
+	void Start () {
+        SimpleGesture.OnTap(MyOnTap);
+	}
+
+    void MyOnTap()
+    {
+        Debug.Log("Tap");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        infoText.text = "No input detected";
+
+        List<Vector2> touchCoordinates = new List<Vector2>();
+
+        //actual device touches
+        foreach(Touch touch in Input.touches)
+        {
+            touchCoordinates.Add(touch.position);
+        }
+
+        //dummy touches
+       if (Input.GetMouseButton(0))
+        {
+            touchCoordinates.Add(Input.mousePosition);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            touchCoordinates.Add(new Vector2(42, 42));
+        }
+
+        if (Input.GetKey(KeyCode.V))
+        {
+            touchCoordinates.Add(new Vector2(48, 48));
+        }
+ 
+        if(touchCoordinates.Count > 0)
+        {
+            infoText.text = "";
+            for (int i = 0; i < touchCoordinates.Count; i++)
+            {
+                infoText.text += string.Format("Input {0}: {1}, {2}\n", i + 1, touchCoordinates[i].x, touchCoordinates[i].y);
+            }
+        }
+
+        if(touchCoordinates.Count == 2)
+        {
+            player.ActivateInvincibility();
+        } else if (touchCoordinates.Count == 3)
+        {
+            player.ActivateSpeedUp();
+        }
+    }
+}
